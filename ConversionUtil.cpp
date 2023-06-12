a0b6dba36392f4c875cae67bf8c22fca4cfdc4af
NikiciaDev <nikiciayt@gmail.com>
2023-06-12 22:19:55 +0200

    fixed a little bug

--- ConversionUtil.cpp
+++ ConversionUtil.cpp
@@ -5,7 +5,7 @@ void cul::convertToAscii(unsigned char* brightness, const unsigned int arraySize
 	double percentage = 0;
 
 	for (unsigned int i = 0; i < arraySize; i++) {
-		percentage = ((short) brightness[i]) / 255.0;
+		percentage = std::round(((short) brightness[i]) / 255.0);
 		index = percentage * (tableSize - 1);
 		brightness[i] = (useReversedAsciiTable ? asciiTable[tableSize - 1 - index] : asciiTable[index]);
 	}
