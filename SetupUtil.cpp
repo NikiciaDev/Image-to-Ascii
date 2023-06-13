1686e6799cc01276fed6313617b72e8dc08e40de
NikiciaDev <nikiciayt@gmail.com>
2023-06-13 16:58:45 +0200

    cleanup

--- SetupUtil.cpp
+++ SetupUtil.cpp
@@ -20,19 +20,14 @@ namespace sul {
 			std::cout << "[ITA] Path seems to be invalid!\n";
 			return false;
 		}
+
 		std::cout << "[ITA] Please input the desired brightness calculation algorithm. (0 - 2)\n";
 		std::cin >> brightnessCalculationAlgorithm;
 		if (brightnessCalculationAlgorithm < 0 || brightnessCalculationAlgorithm > 3) {
 			std::cout << "[ITA] Invalid brightness calculation algorithm!\n";
 			return false;
 		}
-		std::cout << "[ITA] Would you like to use a brightness reversed ascii table? (Y/N)\n";
-		std::cin >> tempInput;
-		if (tempInput != "y" && tempInput != "Y" && tempInput != "n" && tempInput != "N") {
-			std::cout << "[ITA] Please either input \"Y\" or \"N\"!\n";
-			return false;
-		}
-		useReversedAsciiTable = (tempInput == "y" || tempInput == "Y");
+
 		std::cout << "[ITA] Please specify the rounding percision. (-1 = no rounding, 0.001 = round to thrid decimal)\n";
 		std::cin >> tempInput;
 		roundingPercision = strtod(tempInput.c_str(), &end);
@@ -41,6 +36,22 @@ namespace sul {
 			std::wcout << "[ITA] Please enter a valid percision value!\n";
 			return false;
 		}
+
+		std::cout << "[ITA] What should the resolution divisor be? (-1 = no divisor)\n";
+		std::cin >> pixelsPerPixel;
+		if (pixelsPerPixel != -1 && (pixelsPerPixel < 4 || pixelsPerPixel % 2 != 0)) {
+			std::cout << "[ITA] Invalid divisor!\n";
+			return false;
+		}
+
+		std::cout << "[ITA] Would you like to use a brightness reversed ascii table? (Y/N)\n";
+		std::cin >> tempInput;
+		if (tempInput != "y" && tempInput != "Y" && tempInput != "n" && tempInput != "N") {
+			std::cout << "[ITA] Please either input \"Y\" or \"N\"!\n";
+			return false;
+		}
+		useReversedAsciiTable = (tempInput == "y" || tempInput == "Y");
+
 		std::cout << "[ITA] Would you like to copy the string to your clipboard? (Y/N)\n";
 		std::cin >> tempInput;
 		if (tempInput != "y" && tempInput != "Y" && tempInput != "n" && tempInput != "N") {
@@ -48,12 +59,7 @@ namespace sul {
 			return false;
 		}
 		copyToClipboard = (tempInput == "y" || tempInput == "Y");
-		std::cout << "[ITA] What should the resolution divisor be? (-1 = no divisor)\n";
-		std::cin >> pixelsPerPixel;
-		if (pixelsPerPixel != -1 && (pixelsPerPixel < 4 || pixelsPerPixel % 2 != 0)) {
-			std::cout << "[ITA] Invalid divisor!\n";
-			return false;
-		}
+
 		return true;
 	}
 }
